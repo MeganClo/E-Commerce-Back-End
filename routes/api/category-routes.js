4083634ea4ddd1bda60060648751981d95f6d144
Megs <Megs.close@gmail.com>
2021-04-26 12:31:40 -0700

    getting some category routes running and tested

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,12 +5,37 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+  // be sure to include its associated Products  
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+      // be sure to include its associated Products
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.post('/', (req, res) => {
